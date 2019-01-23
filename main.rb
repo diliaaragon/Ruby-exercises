7c86ebe042d0db8d04daab5fb34499abf5c88731
diliaaragon <dilia.aragon@koombea.com>
2019-01-23 11:08:36 -0500

    Change the name of the method, and remove a condition

--- main.rb
+++ main.rb
@@ -2,6 +2,6 @@ require './maths'
 
 puts "Write your word:"
 word = gets.chomp
-palindrome_word = Maths.new.palindrome_word(word)
+palindrome_word = Maths.new.palindrome?(word)
 puts "The word is not palindrome" if palindrome_word == false
 puts "The word is palindrome" if palindrome_word == true
--- maths.rb
+++ maths.rb
@@ -1,11 +1,9 @@
 class Maths
-  def palindrome_word(word)
-      palindrome = false
-     if word.reverse == word
-       palindrome = true
-     else 
-      palindrome = false
-     end
-     return palindrome
-  end
+  def palindrome?(word)
+   palindrome = false
+   if word.reverse == word
+      palindrome = true
+   end
+    return palindrome
+ end
 end
