9cad858894ad2444b5e6094eca83063f7201af69
diliaaragon <dilia.aragon@koombea.com>
2019-01-23 11:40:46 -0500

    Change the name of the variables and replace the print by puts

--- main.rb
+++ main.rb
@@ -1,14 +1,14 @@
 require './maths'
 
-sums = Maths.new.multiples_of_3_and_5
+sums = Maths.new.multiples_of_3_and_5(1000)
 
-print " quiere ver la suma de los multiplos de 5 o 3: "
+puts 'quiere ver la suma de los multiplos de 5 o 3:'
 suma = gets.chomp.to_i
 
 if suma == 5
-  print "la suma de los multiplos de 5 es: #{sums[1]}"
+  puts "la suma de los multiplos de 5 es: #{sums[1]}"
 elsif suma == 3
-  print "la suma de los multiplos de 3 es: #{sums[0]}"
+  puts "la suma de los multiplos de 3 es: #{sums[0]}"
 else
-  print "esa opcion no esta disponible"
+  puts 'esa opcion no esta disponible'
 end
--- maths.rb
+++ maths.rb
@@ -1,14 +1,13 @@
 class Maths
-  def multiples_of_3_and_5()
-    suma3 = 0
-    suma5 = 0
-    (0..1000).each do |i|
+  def multiples_of_3_and_5(num)
+    sum3, sum5 = 0, 0
+    (0..num).each do |i|
       if i % 3 == 0
-        suma3 += i
+        sum3 += i
       elsif i % 5 == 0
-        suma5 += i
+        sum5 += i
       end  
     end 
-    return suma3, suma5 #[3, 5]
+    return sum3, sum5 
   end
 end
