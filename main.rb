0a8ce007b43efb728a83d1573f14bacc8ea742d5
diliaaragon <dilia.aragon@koombea.com>
2019-01-28 10:55:26 -0500

    Change the language of outputs

--- main.rb
+++ main.rb
@@ -3,19 +3,19 @@ require './maths'
 math = Maths.new
 sums = math.multiples_of_3_and_5(1000)
 
-puts 'Quiere ver la suma de los multiplos de 5 o 3:'
+puts 'Do you want to see the sum of multiples of 5 or 3?'
 suma = gets.chomp.to_i
 
 if suma == 5
-  puts "La suma de los multiplos de 5 es: #{sums[1]}"
+  puts "The sum of the multiples of 5 is: #{sums[1]}"
 elsif suma == 3
-  puts "La suma de los multiplos de 3 es: #{sums[0]}"
+  puts "The sum of the multiples of 3 is: #{sums[0]}"
 else
-  puts 'Esa opcion no esta disponible'
+  puts 'That option is not available'
 end
 
 sum = math.even_fibonacci_sum(4000000)
-puts "La suma de los pares del fibonacci #{sum}"
+puts "The sum of the even numbers of fibonacci: #{sum}"
 
 prime_largest = math.largest_prime_factor(600851475143)
-puts " The largest prime #{prime_largest}"
+puts "The largest prime #{prime_largest}"
